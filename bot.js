3ff11d7feacb58c9cec73b7076874ffb77d08106
Hewl56 <62257953+Hewl56@users.noreply.github.com>
2020-03-16 20:22:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,8 +31,8 @@ client.on('ready', () => {
 
 
 
-const adminprefix = "!";
-const developers = ['518468852976451614'];
+const adminprefix = ">";
+const developers = ['586563026010898478'];
 
 console.log("FOIL");
 
